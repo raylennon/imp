942f98c225018832292bdc76b9c027828192510c
Ray Lennon <raylennon27@gmail.com>
2024-03-17 18:11:27 -0400

    some renaming

---
+++ harmony/dist/assets/gyroid_shader-C668xIip.js
@@ -0,0 +1,90 @@
+const r=`uniform vec2 u_resolution;\r
+uniform vec4 u_orientation;\r
+uniform vec3 u_position;\r
+uniform float u_time;\r
+uniform float u_FL;\r
+\r
+uniform sampler2D matcapTexture; // Your matcap texture\r
+\r
+\r
+float udBox( vec3 p, vec3 b )\r
+{\r
+  return length(max(abs(p)-b,0.0));\r
+}\r
+\r
+vec2 matcap(vec3 eye, vec3 normal) {\r
+  vec3 reflected = reflect(-eye, normal);\r
+  float m = 2.8284271247461903 * sqrt( abs(reflected.y)+1.0 );\r
+  return reflected.xz / m + 0.5;\r
+}\r
+\r
+float or = u_orientation.w;\r
+float ox = u_orientation.x;\r
+float oz = u_orientation.y;\r
+float oy = -u_orientation.z;\r
+float s = 1.0 / (or*or + ox*ox + oy*oy + oz*oz);\r
+\r
+mat3 rot = mat3(\r
+    1.0 - 2.0*s*(oy*oy+oz*oz), 2.0 * s * (ox*oy - oz*or), 2.0 * s * (ox*oz + oy*or),\r
+		2.0 * s * (ox*oy + oz*or), 1.0 - 2.0*s*(ox*ox+oz*oz), 2.0 * s * (oy*oz - ox*or),\r
+		2.0 * s * (ox*oz - oy*or), 2.0 * s * (oy*oz + ox*or), 1.0 - 2.0*s*(ox*ox+oy*oy)\r
+);\r
+\r
+vec3 grad (vec3 v) {\r
+  float e = 0.001;\r
+  float local = domain2(v,u_time);\r
+  vec3 g = vec3(\r
+    (domain2(vec3(v.x+e,v.yz),u_time)-local)/(1.0*e),\r
+    (domain2(vec3(v.x,v.y+e,v.z),u_time)-local)/(1.0*e),\r
+    (domain2(vec3(v.xy,v.z+e),u_time)-local)/(1.0*e)\r
+  );\r
+\r
+  g = g/length(g);\r
+  return g;\r
+}\r
+\r
+void main() {\r
+\r
+  vec2 uv = 2.0* gl_FragCoord.xy / u_resolution - 1.0;\r
+  uv.x *= u_resolution.x/u_resolution.y;\r
+  float time = u_time;\r
+  \r
+  float MAX_DIST = 24.0;\r
+  const int MAX_STEPS = 60;\r
+\r
+  vec3 loc = u_position.xzy;\r
+  loc.y = -loc.y;\r
+  vec3 dir =  vec3(uv.x, (u_resolution.x/u_resolution.y) * (u_FL/35.0)*2.0 , uv.y) * rot;\r
+\r
+  vec3 base_color = vec3(1.0, 1.0, 1.0);\r
+  float dist = 0.0;\r
+\r
+  float step_fac = 0.6/length(dir);\r
+\r
+  float current_sign = sign(domain2(loc,u_time));\r
+\r
+for(int i = 0; i < MAX_STEPS; i++){\r
+    if (sign(domain2(loc,u_time)) != current_sign) {\r
+      break;\r
+    }\r
+    dist += abs(step_fac * domain2(loc,u_time));\r
+    loc = loc + step_fac*dir*domain2(loc,u_time);\r
+  }\r
+\r
+  float val = exp(-0.05*dist)/1.0;\r
+  // float val = 1.0;\r
+  \r
+    float gx = mod(loc.x, 3.1415926/2.0);\r
+    float gy = mod(loc.y, 3.1415926/2.0);\r
+    float gz = mod(loc.z,3.1415926/2.0);\r
+    float bt = 0.1;\r
+    if (( (-bt < gx && bt > gx) || (-bt < gy && bt > gy) || (-bt < gz && bt > gz) )) {\r
+    	val *= 0.95;\r
+    }\r
+\r
+  vec3 g = grad(loc);\r
+\r
+  float val1 = 0.5;\r
+  float val2 = 0.5;//(g.x +2.0)/2.0;\r
+  gl_FragColor = vec4(val*val2, val*val2, val*val2, 1.0);\r
+  }`;export{r as default};
---
+++ harmony/graphics.js
@@ -0,0 +1,201 @@
+// graphics.js
+window.mat3 = glMatrix.mat3;
+window.vec3 = glMatrix.vec3;
+
+class GraphicsRenderer {
+  constructor(canvasId, fragmentShaderPath) {
+    this.canvas = document.getElementById(canvasId);
+    this.gl = this.canvas.getContext('webgl');
+
+    if (!this.gl) {
+      console.error('Unable to initialize WebGL. Your browser may not support it.');
+      return;
+    }
+
+    this.setupCanvas();
+    this.loadShaders(fragmentShaderPath);
+  }
+
+  setupCanvas() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+
+    this.gl.canvas.width = this.canvas.clientWidth / 2;
+    this.gl.canvas.height = this.canvas.clientHeight / 2;
+
+    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  }
+
+  async loadShaders(fragmentShaderPath) {
+    const vertexShaderSource = `
+        attribute vec2 a_position;
+        void main() {
+          gl_Position = vec4(a_position, 0.0, 1.0);
+        }
+      `;
+
+    const fragmentShaderSource = await loadShaderFiles();
+
+    this.vertexShader = this.compileShader(vertexShaderSource, this.gl.VERTEX_SHADER);
+    this.fragmentShader = this.compileShader(fragmentShaderSource, this.gl.FRAGMENT_SHADER);
+
+    if (!this.vertexShader || !this.fragmentShader) {
+      console.error('Failed to compile shaders.');
+      return;
+    }
+
+    this.setupProgram();
+  }
+
+  async loadShaderSource(shaderPath) {
+    const response = await fetch(shaderPath);
+    return await response.text();
+  }
+
+  compileShader(source, type) {
+    const shader = this.gl.createShader(type);
+    this.gl.shaderSource(shader, source);
+    this.gl.compileShader(shader);
+
+    if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+      console.error('Shader compilation error:', this.gl.getShaderInfoLog(shader));
+      this.gl.deleteShader(shader);
+      return null;
+    }
+
+    return shader;
+  }
+
+  setupProgram() {
+    this.program = this.gl.createProgram();
+    this.gl.attachShader(this.program, this.vertexShader);
+    this.gl.attachShader(this.program, this.fragmentShader);
+    this.gl.linkProgram(this.program);
+
+    if (!this.gl.getProgramParameter(this.program, this.gl.LINK_STATUS)) {
+      console.error('Program linking error:', this.gl.getProgramInfoLog(this.program));
+      return;
+    }
+
+    this.gl.useProgram(this.program);
+
+    this.setupBuffers();
+    this.setupUniforms();
+    //   this.render();
+  }
+
+  setupBuffers() {
+    const positionAttributeLocation = this.gl.getAttribLocation(this.program, 'a_position');
+    const positionBuffer = this.gl.createBuffer();
+
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+
+    const positions = [
+      -1.0, -1.0,
+      1.0, -1.0,
+      -1.0, 1.0,
+      1.0, 1.0,
+    ];
+
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.STATIC_DRAW);
+
+    this.gl.enableVertexAttribArray(positionAttributeLocation);
+    this.gl.vertexAttribPointer(positionAttributeLocation, 2, this.gl.FLOAT, false, 0, 0);
+  }
+
+  setupUniforms() {
+    const resolutionUniformLocation = this.gl.getUniformLocation(this.program, 'u_resolution');
+    this.gl.uniform2f(resolutionUniformLocation, this.canvas.width, this.canvas.height);
+
+    this.timeUniformLocation = this.gl.getUniformLocation(this.program, 'u_time');
+
+    this.orientationUniformLocation = this.gl.getUniformLocation(this.program, 'u_orientation');
+    this.gl.uniform4fv(this.orientationUniformLocation, [1.0, 0.0, 1.0, 0.0]);
+
+    this.focalLengthUniformLocation = this.gl.getUniformLocation(this.program, 'u_FL');
+    this.gl.uniform1f(this.focalLengthUniformLocation, [12.0]);
+
+    this.positionUniformLocation = this.gl.getUniformLocation(this.program, 'u_position');
+    this.gl.uniform3fv(this.positionUniformLocation, [0.0, 1.0, 0.0]);
+
+    this.matcapTexture = this.gl.createTexture();
+    this.gl.bindTexture(this.gl.TEXTURE_2D, this.matcapTexture);
+
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+
+    var image = new Image();
+    this.matcapTexture = this.gl.createTexture(); // Create the texture object
+
+    image.onload = () => {
+      this.gl.bindTexture(this.gl.TEXTURE_2D, this.matcapTexture);
+      this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, image);
+      this.gl.generateMipmap(this.gl.TEXTURE_2D); // Optionally generate mipmaps
+    };
+    image.src = './657274_2A2F30_A0A7B0_363C44-128px.png'; // Replace this with your texture's path
+
+    this.matcapTextureLocation = this.gl.getUniformLocation(this.program, 'matcapTexture');
+    this.gl.uniform1i(this.matcapTextureLocation, 0); // Use texture unit 0
+
+  }
+
+  render() {
+    // console.log(camera.getWorldDirection());
+    const time = performance.now() / 1000.0;
+    this.gl.uniform1f(this.timeUniformLocation, time);
+
+    // console.log(camera.rotatwssion);
+    viewEuler.copy(camera.rotation);
+    // viewEuler = viewEuler.reorder('XYZ');
+    viewQuat.setFromEuler(viewEuler);
+    this.gl.uniform4fv(this.orientationUniformLocation, viewQuat.toArray());
+
+    // console.log(camera.getFocalLength());
+    this.gl.uniform1fv(this.focalLengthUniformLocation, Float32Array.from([camera.getFocalLength()]));
+    // console.log(gs.position);
+    this.gl.uniform3fv(this.positionUniformLocation, camera.position.toArray());
+    // this.gl.uniform3fv(this.positionUniformLocation, position);
+
+    // In your rendering loop:
+    this.gl.activeTexture(this.gl.TEXTURE0);
+    this.gl.bindTexture(this.gl.TEXTURE_2D, this.matcapTexture);
+
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+
+    requestAnimationFrame(() => this.render());
+  }
+
+}
+
+// Load the shader files asynchronously
+async function loadShaderFiles() {
+  // const shader1Response = await fetch('./domain.fs');
+  // const shader1Text = await shader1Response.text();
+  const shader1Text = (await import('./domain.fs?raw')).default;
+  
+  // const shader2Response = await fetch('./gyroid_shader.fs');
+  // const shader2Text = await shader2Response.text();
+  const shader2Text = (await import('./gyroid_shader.fs?raw')).default;
+
+  // Combine the shader code from both files
+  const combinedShaderCode = shader1Text + '\n' + shader2Text;
+    // console.log(shader2Text);
+
+  return combinedShaderCode;
+}
+window.startGraphics = async function () {
+  try {
+    // Load both shader files and combine them
+    const combinedShaderCode = await loadShaderFiles();
+
+    // Initialize GraphicsRenderer with the combined shader code
+    const gl_renderer = new GraphicsRenderer('webgl-canvas', combinedShaderCode);
+    gl_renderer.render();
+  } catch (error) {
+    console.error('Error initializing GraphicsRenderer:', error);
+  }
+}
