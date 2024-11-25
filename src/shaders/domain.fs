fd0e90b372a1414dcbb899accefed61c4eba4127
Ray Lennon <48305386+raylennon@users.noreply.github.com>
2024-11-25 08:56:19 -0500

    Update domain.fs

--- src/shaders/domain.fs
+++ src/shaders/domain.fs
@@ -1,6 +1,6 @@
 
 float domain(vec3 v, float t) {
-	      float cube = box(v, vec3(3.0));
+	float cube = box(v, vec3(3.0));
         float fillet_plane = (v.x+v.y-4.0)/2.;
         fillet_plane -= 0.6*sin(t);
         cube = max(cube, fillet_plane);
