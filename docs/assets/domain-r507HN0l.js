9c1dd1fac86debf504ffbada68f530da55c71382
Ray Lennon <48305386+raylennon@users.noreply.github.com>
2024-11-25 09:03:32 -0500

    Update domain-r507HN0l.js

--- docs/assets/domain-r507HN0l.js
+++ docs/assets/domain-r507HN0l.js
@@ -1,4 +1,8 @@
-const a=`float domain(vec3 v, float t) {\r
-  float val1 = sphere(v-vec3(0.0,0.0,0.0), 4.0);\r
-  return smin2(val1, box(v-vec3(3, 3, 3), vec3(2.0, 2.0, 2.0)), 0.2);\r
-}`;export{a as default};
+const a=`\r
+float domain(vec3 v, float t) {\r
+	float cube = box(v, vec3(3.0));\r
+        float fillet_plane = (v.x+v.y-4.0)/2.;\r
+        fillet_plane -= 0.6*sin(t);\r
+        cube = max(cube, fillet_plane);\r
+        return cube;\r
+}\r`;export{a as default};
